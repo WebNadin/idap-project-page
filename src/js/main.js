475e3c48462ce901b8b75db2dcd271aefa2d830f
WebNadin <nenadinweb@gmail.com>
2019-04-14 20:08:42 +0300

    tabs were created and work

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,29 @@
 $(document).ready(function () {
     $('select').niceSelect();
+    switchTabs();
     /*alert("ok");*/
     /*$(".slogan").css("color", "red");*/
     /*var $width = $(document).width();
-    if ($width < 767) {
-    }
-    else {
-    }*/
+     if ($width < 767) {
+     }
+     else {
+     }*/
 
 });
+
+function switchTabs() {
+    $("#content .content__item").hide(); // Скрываем содержание
+    $("#tabs .nn-tabs__item:first").attr("id", "current"); // Активируем первую закладку
+    $("#content .content__item:first").fadeIn(); // Выводим содержание
+
+    $('#tabs a').click(function (e) {
+        e.preventDefault();
+        $("#content .content__item").hide(); //Скрыть все сожержание
+        $("#tabs .nn-tabs__item").attr("id", ""); //Сброс ID
+        $(this).parent().attr("id", "current"); // Активируем закладку
+        $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
+    });
+}
 
 
 
