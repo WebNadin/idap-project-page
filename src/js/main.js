bc843c0a2aef0b4f85733a1c22ebab6a45e023a3
WebNadin <nenadinweb@gmail.com>
2019-04-14 20:29:48 +0300

    main section structure was created

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,13 @@ $(document).ready(function () {
 });
 
 function switchTabs() {
-    $("#content .content__item").hide(); // Скрываем содержание
+    $("#content .b-content__item").hide(); // Скрываем содержание
     $("#tabs .nn-tabs__item:first").attr("id", "current"); // Активируем первую закладку
-    $("#content .content__item:first").fadeIn(); // Выводим содержание
+    $("#content .b-content__item:first").fadeIn(); // Выводим содержание
 
     $('#tabs a').click(function (e) {
         e.preventDefault();
-        $("#content .content__item").hide(); //Скрыть все сожержание
+        $("#content .b-content__item").hide(); //Скрыть все сожержание
         $("#tabs .nn-tabs__item").attr("id", ""); //Сброс ID
         $(this).parent().attr("id", "current"); // Активируем закладку
         $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
