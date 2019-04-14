6b1ffbb026640205cbdf0071bc912279fdde2b8f
WebNadin <nenadinweb@gmail.com>
2019-04-15 00:33:26 +0300

    switch between tabs was fixed

--- src/js/main.js
+++ src/js/main.js
@@ -11,15 +11,15 @@ $(document).ready(function () {
 
 function switchTabs() {
     $("#content .b-content__item").hide(); // Скрываем содержание
-    $("#tabs .nn-tabs__item:first").attr("id", "current"); // Активируем первую закладку
+    $("#tabs .js-link_tabs:first").attr("id", "current"); // Активируем первую закладку
     $("#content .b-content__item:first").fadeIn(); // Выводим содержание
 
     $('#tabs a').click(function (e) {
         e.preventDefault();
         $("#content .b-content__item").hide(); //Скрыть все сожержание
-        $("#tabs .nn-tabs__item").attr("id", ""); //Сброс ID
-        $(this).parent(".nn-tabs__item").toggleClass("active");
-        $("#tabs a").not(this).parent(".nn-tabs__item").toggleClass("active");
+        $("#tabs .js-link_tabs").attr("id", ""); //Сброс ID
+        $(this).closest(".nn-tabs__item").toggleClass("active");
+        $("#tabs a").not(this).closest(".nn-tabs__item").toggleClass("active");
         $(this).parent().attr("id", "current"); // Активируем закладку
         $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
     });
